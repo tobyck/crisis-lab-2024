5d5e71f52c3a2733a0d0e8355606b1455d864152
AlexBerry0 <me@alex-berry.net>
2024-05-07 15:49:33 +1200

    Added an Arduino demo

---
+++ demos/alex-demo.ino
@@ -0,0 +1,49 @@
+#include <SparkFun_LPS28DFW_Arduino_Library.h>
+#include <Wire.h>
+
+// Create a new sensor object
+LPS28DFW pressureSensor;
+
+// I2C address selection
+uint8_t i2cAddress = LPS28DFW_I2C_ADDRESS_DEFAULT; // 0x5C
+//uint8_t i2cAddress = LPS28DFW_I2C_ADDRESS_SECONDARY; // 0x5D
+
+void setup()
+{
+    // Start serial
+    Serial.begin(115200);
+    Serial.println("LPS28DFW Example 1 - Basic Readings! I AM GOING TO KMS IF THIS FUCKING WORKS");
+
+    // Initialize the I2C library
+    Wire.begin();
+
+    // Check if sensor is connected and initialize
+    // Address is optional (defaults to 0x5C)
+    while(pressureSensor.begin(i2cAddress) != LPS28DFW_OK)
+    {
+        // Not connected, inform user
+        Serial.println("Error: LPS28DFW not connected, check wiring and I2C address!");
+
+        // Wait a bit to see if connection is established
+        delay(1000);
+    }
+
+    Serial.println("LPS28DFW connected!");
+}
+
+void loop()
+{
+    // Get measurements from the sensor. This must be called before accessing
+    // the pressure data, otherwise it will never update
+    pressureSensor.getSensorData();
+
+    // Print temperature and pressure
+    Serial.print("Temperature (C): ");
+    Serial.print(pressureSensor.data.heat.deg_c);
+    Serial.print("\t\t");
+    Serial.print("Pressure (hPa): ");
+    Serial.println(pressureSensor.data.pressure.hpa);
+
+    // Only print every second
+    delay(1000);
+}
