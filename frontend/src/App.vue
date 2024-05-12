0b4e6d18555839e7f40fa19d05b8960f8cdb0dd0
AlexBerry0 <me@alex-berry.net>
2024-05-12 14:00:39 +1200

    Add back in footer which was accidentaly deleated in the last commit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -41,6 +41,7 @@
             :data-source="pressure"
         />
   </div>
+  <Footer />
 </template>
 
 <style scoped>
@@ -85,6 +86,7 @@ div.padding {
 <script setup>
 import Header from './components/Header.vue';
 import Chart from './components/Chart.vue';
+import Footer from './components/Footer.vue';
 import { ref, computed } from 'vue';
 import { packetData, initWebsocket, loaded } from './ws.js';
 
