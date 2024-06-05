72e0741aa6486705ddeac16669bb9559ae3ce7ee
tobyck <toby_ck@icloud.com>
2024-06-05 13:35:28 +1200

    Add a Nix derivation for the relay server

--- flake.nix
+++ flake.nix
@@ -1,15 +1,22 @@
 {
 	description = "WHS CRISiSLab Challenge 2024 project";
 
-	inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-	inputs.flake-utils.url = "github:numtide/flake-utils";
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+		flake-utils.url = "github:numtide/flake-utils";
+		naersk.url = "github:nix-community/naersk";
+	};
 
-	outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: let
+	outputs = { self, nixpkgs, flake-utils, naersk }: flake-utils.lib.eachDefaultSystem (system: let
 	  pkgs = import nixpkgs { inherit system; };
+		naersk' = pkgs.callPackage naersk {};
 	in {
-		devShells = {
-			relay = pkgs.mkShell {
+		packages = {
+			relay = naersk'.buildPackage {
+				src = ./backend/relay;
+
 				nativeBuildInputs = with pkgs; [
+					cargo
 					pkg-config
 					cmake
 					openssl
