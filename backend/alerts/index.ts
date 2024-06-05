3b0dadfd6a75261dd3054e0e80571ee237b82073
Chunkybanana <chunkybananagithub@gmail.com>
2024-06-06 08:39:03 +1200

    added password I guess

--- backend/alerts/index.ts
+++ backend/alerts/index.ts
@@ -1,9 +1,19 @@
-import { serve, sha } from "bun"
+import { serve } from "bun"
 import { sendEmail, addEmail, removeEmail } from "./mailer";
 import { postInstagram } from "./instagram";
+import { timingSafeEqual } from "crypto";
 
 // yes, seriously
 const EMAILREGEX = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+
+let safeCompare = (a: string, b: string) => {
+    if (a.length < b.length) [a, b] = [b, a];
+    let result = 0;
+    for (let i = 0; i < a.length; i++) {
+        result |= a.charCodeAt(i) ^ (b.charCodeAt(i) ?? 0);
+    }
+    return result === 0;
+}
 
 serve({
     port: 8783,
@@ -37,8 +47,20 @@ serve({
             // TODO: password
 
             let json = await req.json();
+            if (json.password === undefined || typeof json.password !== "string") {
+                return new Response("No password provided", { status: 400 });
+            }
+
+            if (!safeCompare(json.password, process.env.ALERT_PASSWORD as string)) {
+                return new Response("Incorrect password", { status: 401 });
+            }
+
             if (json.height === undefined) {
                 return new Response("No height provided", { status: 400 });
+            }
+
+            if (typeof json.height !== "number") {
+                return new Response("Invalid height", { status: 400 });
             }
 
             let message = `WARNING A FAKE TSUNAMI OF HEIGHT ${json.height}cm HAS BEEN RECORDED`;
