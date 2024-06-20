992a6dd083c072ed2d27f9f1e4724f71e89babf0
Chunkybanana <chunkybananagithub@gmail.com>
2024-06-21 10:10:48 +1200

    cleaned up discord

---
+++ backend/alerts/src/discord.ts
@@ -0,0 +1,24 @@
+import { Events, Client, TextChannel } from "discord.js";
+
+const client = new Client({
+    intents: ["Guilds", "GuildMessages", "DirectMessages"],
+});
+client.login(process.env.DISCORD_TOKEN)
+
+let channel: TextChannel | null = null;
+
+client.once(Events.ClientReady, readyClient => {
+    console.log('Discord bot ready');
+    channel = readyClient.channels.cache.find(channel =>
+        channel instanceof TextChannel && channel.name === 'tsunami-alerts'
+    ) as TextChannel;
+    postDiscord('bot up');
+})
+
+export const postDiscord = async (message: string) => {
+    if (channel === null) {
+        console.error('Channel not found');
+        return;
+    }
+    await channel.send(message);
+}
