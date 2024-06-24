e46fe63e5278ec8a2e617e633e5d1c8f36d54521
tobyck <toby_ck@icloud.com>
2024-06-24 12:17:55 +1200

    Back to nightly Rust (fixed the libssl issue)
    libtest for benchmarking doesn't work on stable Rust

--- flake.nix
+++ flake.nix
@@ -3,33 +3,76 @@
 
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
 		flake-utils.url = "github:numtide/flake-utils";
 		naersk.url = "github:nix-community/naersk";
+
+		nixpkgs-mozilla = {
+			url = "github:mozilla/nixpkgs-mozilla";
+			flake = false;
+		};
 	};
 
-	outputs = { self, nixpkgs, flake-utils, naersk }: flake-utils.lib.eachDefaultSystem (system: let
-	  pkgs = import nixpkgs { inherit system; };
-		naersk' = pkgs.callPackage naersk {};
+	outputs = {
+		self,
+		nixpkgs,
+		flake-utils,
+		naersk,
+		nixpkgs-mozilla
+	}: flake-utils.lib.eachDefaultSystem (system: let
+		pkgs = import nixpkgs {
+			inherit system;
+			overlays = [ (import nixpkgs-mozilla) ];
+		};
+
+		toolchain = (pkgs.rustChannelOf {
+			channel = "nightly";
+			date = "2024-06-13";
+			sha256 = "sha256-s5nlYcYG9EuO2HK2BU3PkI928DZBKCTJ4U9bz3RX1t4=";
+		}).rust;
+
+		naersk' = pkgs.callPackage naersk {
+			cargo = toolchain;
+			rustc = toolchain;
+		};
+
+		relayNativeBuildInputs = with pkgs; [ pkg-config cmake makeWrapper ];
+		relayBuildInputs = [ pkgs.openssl ];
+		libsslPath = pkgs.lib.makeLibraryPath [ pkgs.openssl ];
 	in {
 		packages = {
 			relay = naersk'.buildPackage {
 				src = ./backend/relay;
 
-				nativeBuildInputs = with pkgs; [
-					pkg-config
-					cmake
-					openssl
-				];
+				nativeBuildInputs = relayNativeBuildInputs;
+				buildInputs = relayBuildInputs;
 
-				RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+				postInstall = ''
+					wrapProgram "$out/bin/relay" \
+						--suffix LD_LIBRARY_PATH : ${libsslPath}
+				'';
 			};
 		};
 
-		devShells.default = pkgs.mkShell {
-			buildInputs = with pkgs; [
-				bun
-				mosquitto
-			];
+		devShells = {
+			default = pkgs.mkShell {
+				buildInputs = with pkgs; [
+					bun
+					mosquitto
+				];
+			};
+
+			relay = pkgs.mkShell {
+				buildInputs = 
+					relayNativeBuildInputs ++ 
+					relayBuildInputs ++ 
+					[
+						toolchain
+						pkgs.cargo-flamegraph
+					];
+
+				RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+			};
 		};
 	});
 }
