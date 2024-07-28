d01c8c72b24ccb3d20f20e32ec50c5d898c96f8d
aketon08 <84485391+aketon08@users.noreply.github.com>
2024-07-29 10:33:26 +1200

    Update NotificationModule.kt

--- androidapp/Crisislab/app/src/main/kotlin/com/example/crisislab/NotificationModule.kt
+++ androidapp/Crisislab/app/src/main/kotlin/com/example/crisislab/NotificationModule.kt
@@ -1,3 +1,9 @@
+/*
+ * Author: Maxwell Robati
+ * Version: 29/07/2024
+ * Purpose: Container for all notifications
+ */
+
 package com.example.crisislab
 
 import android.app.Notification
@@ -11,13 +17,13 @@ import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 
 object NotificationModule {
-
     // Function to build a notification
     fun build(context: Context, title: String, message: String, bigText: String?): Notification {
         // Intent to open the app when the user taps the notification
         val intent = Intent(context, MainActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         }
+        
         val pendingIntent: PendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE) // Opens the app when triggered
 
         // Intent for full-screen notifications
@@ -26,13 +32,12 @@ object NotificationModule {
 
         // Build and return the notification
         return NotificationCompat.Builder(context, "Main Channel ID")
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .setSmallIcon(R.drawable.ic_notif)
             .setContentTitle(title)
             .setContentText(message)
-            .setStyle(NotificationCompat.BigTextStyle().bigText(bigText))  // Set the style for the notification with big text
+            .setStyle(NotificationCompat.BigTextStyle().bigText(bigText))  // Sets the text when the notification is dropped down
             .setPriority(NotificationCompat.PRIORITY_HIGH)  // Allows the notification to be a heads up one
-            .setContentIntent(pendingIntent) // Triggers pendingIntent when the user clicks on the notification
+            .setContentIntent(pendingIntent) // Opens the app when the user clicks on the notification
             .setFullScreenIntent(pendingFullScreenIntent, true)
             .setAutoCancel(true)  // Automatically remove the notification when the user taps it
             .setOngoing(true)
@@ -40,10 +45,10 @@ object NotificationModule {
             .build()
     }
 
-    // Function to provide the NotificationManagerCompat
+    // Function to generate a Notification Manager
     fun provideNotificationManager(context: Context): NotificationManagerCompat {
         val notificationManager = NotificationManagerCompat.from(context)
-        // Create a notification channel for devices running Android O or higher
+        // Create a notification channel for devices running a high enough version of Android
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
                 "Main Channel ID",
