bd7a51e42dfd31723d0e7e2cb2d56e2ce6276db5
aketon08 <84485391+aketon08@users.noreply.github.com>
2024-07-29 10:35:40 +1200

    Update SocketStatusAdapter.kt

--- androidapp/Crisislab/app/src/main/kotlin/com/example/crisislab/SocketStatusAdapter.kt
+++ androidapp/Crisislab/app/src/main/kotlin/com/example/crisislab/SocketStatusAdapter.kt
@@ -1,3 +1,9 @@
+/*
+ * Author: Alex Berry
+ * Version: 29/07/2024
+ * Purpose: Displays the WebSocket's current connectivity status
+ */
+
 package com.example.crisislab
 
 import android.os.Build
@@ -9,13 +15,10 @@ import com.example.crisislab.databinding.StatusCellBinding
 
 // Adapter for displaying socket status in a RecyclerView
 class SocketStatusAdapter(
-    // Status to be displayed
     private val status: String
 ) : RecyclerView.Adapter<SocketStatusViewHolder>() {
-
     // Create and return a ViewHolder for the item
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SocketStatusViewHolder {
-        // Inflate the layout for the item
         val layoutInflater = LayoutInflater.from(parent.context)
         val binding = StatusCellBinding.inflate(layoutInflater, parent, false)
         return SocketStatusViewHolder(binding)
