6dcdee5b7382228145768787f932b2daf92e0204
aketon08 <84485391+aketon08@users.noreply.github.com>
2024-07-29 12:43:31 +1200

    Update LogViewModel.kt

--- androidapp/Crisislab/app/src/main/kotlin/com/example/crisislab/LogViewModel.kt
+++ androidapp/Crisislab/app/src/main/kotlin/com/example/crisislab/LogViewModel.kt
@@ -1,14 +1,15 @@
 /*
  * Author: Alex Berry
  * Version: 29/07/2024
- * Purpose: Manages and edits and creates the list of logs
+ * Purpose: Manages and edits the list of logs
  */
+
 package com.example.crisislab
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-// ViewModel to store and manage log items within the recyler view
+// ViewModel to store and manage log items within the recycler view
 class LogViewModel : ViewModel() {
     var logItems = MutableLiveData<MutableList<LogItem>>(ArrayList())
 
