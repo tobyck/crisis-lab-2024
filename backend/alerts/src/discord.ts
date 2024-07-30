4a2de28304bdfc4c168b319dcc98b192f4c7a22d
Alex Berry <me@alex-berry.net>
2024-07-31 10:44:21 +1200

    Add commenting for my code in alerts section

--- backend/alerts/src/discord.ts
+++ backend/alerts/src/discord.ts
@@ -1,20 +1,27 @@
+/*
+ * Author: Alex Berry & Theo Keith
+ * Version: 31/07/2024
+ * Purpose: Create a discord bot client, and ready it for sending messages
+ */
 import { Events, Client, TextChannel } from "discord.js";
 
 const client = new Client({
-    intents: ["Guilds", "GuildMessages", "DirectMessages"],
+    intents: ["Guilds", "GuildMessages", "DirectMessages"], // Discordjs labels servers as guilds
 });
 client.login(process.env.DISCORD_TOKEN)
 
 let channel: TextChannel | null = null;
 
+// Setup process for when the bot connection is initialised
 client.once(Events.ClientReady, readyClient => {
     console.log('Discord bot ready');
     channel = readyClient.channels.cache.find(channel =>
-        channel instanceof TextChannel && channel.name === 'tsunami-alerts'
+        channel instanceof TextChannel && channel.name === 'tsunami-alerts' // Set the channel to post in to be any thats called tsunami-alerts
     ) as TextChannel;
     postDiscord('Bot online!');
 })
 
+// Error if the bot can't find any channels
 export const postDiscord = async (message: string) => {
     if (channel === null) {
         console.error('Channel not found');
--- backend/alerts/src/instagram.ts
+++ backend/alerts/src/instagram.ts
@@ -1,19 +1,25 @@
+/*
+ * Author: Alex Berry
+ * Version: 31/07/2024
+ * Purpose: Log onto instagram, and make a post with the tsunami icon picture, and the caption
+ */
 import { IgApiClient } from 'instagram-private-api';
 import { readFile } from 'fs';
 import { promisify } from 'util';
 const readFileAsync = promisify(readFile);
 
 export const postInstagram = async (message: string) => {
-    let { IG_USERNAME, IG_PASSWORD } = process.env;
-    if (!IG_USERNAME || !IG_PASSWORD) {
+    let { IG_USERNAME, IG_PASSWORD } = process.env; // Read Instagram username and password from .env file
+    if (!IG_USERNAME || !IG_PASSWORD) { // If they don't exist error
         console.log('Instagram credentials not found');
         return;
     }
     try {
-        const ig = new IgApiClient();
-        ig.state.generateDevice(IG_USERNAME);
-        await ig.account.login(IG_USERNAME, IG_PASSWORD);
+        const ig = new IgApiClient(); // Create a new instagram API client
+        ig.state.generateDevice(IG_USERNAME); // Generate a fake device's metadata to log the instagram connection as coming from
+        await ig.account.login(IG_USERNAME, IG_PASSWORD); // Log into the account
         const path = './tsunami-icon.jpg';
+        // Publish the photo to instagram with the specified caption
         await ig.publish.photo({
             file: await readFileAsync(path),
             caption: message
